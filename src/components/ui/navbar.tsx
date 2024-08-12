41aab0a18c23ae14667ec69f56963941c14fa21c
ChiragGaur13 <chiraggaur321@gmail.com>
2024-08-12 10:20:19 +0530

    Second commit

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -11,55 +11,6 @@ function Navbar() {
     <>
 
     <div className='w-full'>
-      {/* <nav className='flex bg-emerald-800 py-6'>
-        <div className='flex w-full items-center'>
-          <h1 className='text-white text-4xl pl-10 font-semibold xl:pl-20 lg:pl-15 md:pl-20 sm:pl-20 '>Furni</h1>
-          
-
-          <div className='flex justify-end items-center w-full'>
-
-          {
-            show ? (
-              <div className='flex flex-col justify-center items-center'>
-              
-              <BiMenu className='h-11 w-20 md:hidden text-gray-300 ' onClick={clickIcon}/>
-                <ul className=' text-gray-400 gap-8 px-16 flex flex-col md:hidden'>
-                  <li><a className='text-white ' href=''>Home</a></li>
-                  <li><a className='hover:text-white' href=''>Shop</a></li>
-                  <li><a className='hover:text-white' href=''>About us</a></li>
-                  <li><a className='hover:text-white ' href=''>Services</a></li>
-                  <li><a className='hover:text-white ' href=''>Blog</a></li>
-                  <li><a className='hover:text-white ' href=''>Contact us</a></li>
-                </ul>
-                </div>
-
-            ) : (              
-              <BiMenu className='h-11 w-20 md:hidden text-gray-300' onClick={clickIcon}/>
-
-            )
-          }
-          
-            
-            <ul className=' text-gray-400 xl:gap-8 xl:px-16 md:px-10  md:gap-4 hidden md:flex'>
-              <li><a className='text-white border-b-4 p-2  border-yellow-500' href=''>Home</a></li>
-              <li><a className='hover:text-white hover:border-b-4 p-2  border-yellow-500 ' href=''>Shop</a></li>
-              <li><a className='hover:text-white hover:border-b-4 p-2  border-yellow-500' href=''>About us</a></li>
-              <li><a className='hover:text-white hover:border-b-4 p-2  border-yellow-500' href=''>Services</a></li>
-              <li><a className='hover:text-white hover:border-b-4 p-2  border-yellow-500' href=''>Blog</a></li>
-              <li><a className='hover:text-white hover:border-b-4 p-2  border-yellow-500' href=''>Contact us</a></li>
-            </ul>
-            
-            <ul className=' lg:gap-10 gap-5 md:gap-3 pr-10 lg:pr-20 hidden md:flex'>
-              <li><a className='page' href=''><img src={"../public/index.svg"}></img></a></li>
-              <li><a className='page' href=''><img src="../public/cart.svg"></img></a></li>
-            </ul>
-          </div>
-          
-        </div>
-      </nav> */}
-
-
-
 
           {
             show ? (
